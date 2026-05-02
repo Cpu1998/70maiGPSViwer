e1e182c5748a7aa7bf876d8e331b2f8fc8895a00
zhangkeyouAsus <1461375468@qq.com>
2026-05-02 22:09:19 +0800

    增加版本显示

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,12 +5,13 @@ import TripList from './TripList.vue'
 import { useGpsStore } from '../stores/gps'
 
 const store = useGpsStore()
+const version = __APP_VERSION__
 </script>
 
 <template>
   <aside class="sidebar-inner">
     <div class="sidebar-header">
-      <h1 class="logo">70mai GPS 查看器</h1>
+      <h1 class="logo">70mai GPS 查看器 <span class="version">v{{ version }}</span></h1>
       <button v-if="store.summaries.length > 0" class="clear-btn" @click="store.clearAll()" title="清除数据">✕</button>
     </div>
     <FileLoader />
@@ -39,6 +40,11 @@ const store = useGpsStore()
   font-size: 15px;
   font-weight: 700;
   color: #e6edf3;
+}
+.version {
+  font-size: 11px;
+  font-weight: 400;
+  color: #484f58;
 }
 
 .clear-btn {
